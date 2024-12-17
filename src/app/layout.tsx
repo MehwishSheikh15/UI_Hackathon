c8f60b155c689800ae69a0fe35082e907072da23
Mehwish Sheikh <160774324+MehwishSheikh15@users.noreply.github.com>
2024-12-18 00:40:44 +0500

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero"
-import SingleProduct from "./SingleProduct/page";
-import Product from "./Product/page";
-import About from "./AboutUs/page"
-import Cart from "./Cart/page"
-import Contact from "./Contact/page"
-import Faqs from "./Faqs/page"
-import Shop from "./Shop/page"
+
 import Footer from "./component/Footer";
 
 
@@ -17,6 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Comforty",
+  
   
 };
 
@@ -30,13 +26,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         <Hero/>
-        <SingleProduct/>
-        <Product/>
-        <About/>
-        <Cart/>
-        <Contact/>
-        <Faqs/>
-        <Shop/>
         <Footer/>
         {children}</body>
     </html>
